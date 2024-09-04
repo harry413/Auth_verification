70f204473edbeaf5482978181e846b1a2b4d087f
sachin413 <sachinpatidar413@gmail.com>
2024-09-04 21:53:28 +0530

    cross env changes

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,8 @@ import authRoutes from "./routes/auth.route.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
-
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+//process.env.CLIENT_URL
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
